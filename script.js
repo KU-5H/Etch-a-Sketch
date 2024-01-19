ff35a42a2d2075250adecfba08b062eb3e47d9f7
KU-5H <kushpatel76@yahoo.com>
2024-01-19 04:21:44 -0500

    Fixed glitch with grid window in smaller devices and made button in top right toggleable, not yet functional

--- script.js
+++ script.js
@@ -7,3 +7,33 @@ output.innerHTML = slider.value + "x" + slider.value;
 outputLocation.appendChild(output)
 
 slider.oninput = () => {output.innerHTML = slider.value + "x" + slider.value};
+
+//For the dark mode button
+let imageTick = 0;
+const image = document.querySelector('.mode');
+const imageButton = document.querySelector('.modeButton')
+
+imageButton.addEventListener('click', () => {
+    if (imageTick === 0) {
+        image.src = 'images/4808961-200.png'
+        imageTick = 1;
+
+        const lightMode = () => {
+
+        }
+
+        lightMode()
+
+
+    } else {
+        image.src = 'images/5456551-200.png';
+        imageTick = 0;
+
+        const darkMode = () => {
+
+        }
+
+        darkMode()
+    }
+});
+
