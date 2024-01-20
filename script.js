609f3cff0c31e98f92159a82a685c4a86ee52914
KU-5H <kushpatel76@yahoo.com>
2024-01-20 01:02:59 -0500

    Made the grid usable

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//To create text for the slider
+//Slider Properties and text generation based on slider range
 const slider = document.querySelector('#myRange');
 const outputLocation = document.querySelector('.gridSize')
 const output = document.createElement('div');
@@ -6,10 +6,14 @@ const output = document.createElement('div');
 output.innerHTML = slider.value + "x" + slider.value;
 outputLocation.appendChild(output)
 
-slider.oninput = () => {output.innerHTML = slider.value + "x" + slider.value};
+slider.oninput = () => {
+    output.innerHTML = slider.value + "x" + slider.value;
+    document.querySelector('.right-side').innerHTML = ''
+    generateGrid(slider.value);
+};
 
 //For the dark mode button
-let imageTick = 0;
+let imageTick = 1;
 const image = document.querySelector('.mode');
 const imageButton = document.querySelector('.modeButton')
 
@@ -37,3 +41,33 @@ imageButton.addEventListener('click', () => {
     }
 });
 
+
+//To generate a bunch of grids inside the main grid
+function generateGrid(g) {
+
+    const grid = document.querySelector('.right-side');
+    const width = grid.offsetWidth / g;
+    const height = grid.offsetHeight / g;
+
+    console.log(width);
+    console.log(height);
+
+    const area = width * height;
+
+    const numberOfSquares = g*g;
+
+    for(let i = 0; i < numberOfSquares; i++) {
+        const tempDiv = document.createElement('div');
+        tempDiv.classList.add('gridProperties');
+        tempDiv.style.width = width + 'px';
+        tempDiv.style.height = height + 'px';
+
+        grid.appendChild(tempDiv);
+    }
+
+    console.log('this ran');
+
+    return;
+}
+
+generateGrid(slider.value)
